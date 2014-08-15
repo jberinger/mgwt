d20a1ed1d97f2c8120c3e426dabcd6cf7e6b3140
Jürgen Beringer <J.Beringer@gmail.com>
2014-08-15 15:31:04 +0200

    Add TapHandler for CarouselIndicator
    support tap on CarouselIndicator to select the page

--- src/main/java/com/googlecode/mgwt/ui/client/widget/carousel/Carousel.java
+++ src/main/java/com/googlecode/mgwt/ui/client/widget/carousel/Carousel.java
@@ -57,7 +57,7 @@ import java.util.Set;
  */
 public class Carousel extends Composite implements HasWidgets, HasSelectionHandlers<Integer> {
 
-  private static class CarouselIndicatorContainer extends Composite {
+  private class CarouselIndicatorContainer extends Composite {
     private FlexPanel main;
     private final CarouselCss css;
     private ArrayList<CarouselIndicator> indicators;
@@ -87,6 +87,14 @@ public class Carousel extends Composite implements HasWidgets, HasSelectionHandl
         CarouselIndicator indicator = new CarouselIndicator(css);
         indicators.add(indicator);
         container.add(indicator);
+        final int index = i;
+        indicator.addTapHandler(new TapHandler() {
+            
+            @Override
+            public void onTap(TapEvent event) {
+                Carousel.this.setSelectedPage(index);
+            }
+        });
       }
 
       setSelectedIndex(selectedIndex);
